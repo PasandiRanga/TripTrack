2ce487b9c98cc509ba61f591065b535ea2a66c94
ShashiniMadhu <shashinimadushika413@gmail.com>
2024-11-22 23:12:24 +0530

    create
    create css of some pages

--- App/controllers/GuestPages.php
+++ App/controllers/GuestPages.php
@@ -123,9 +123,9 @@
                 // Validate NIC
                 if (empty($data['nic'])) {
                     $data['nic_err'] = 'Please enter a NIC';
-                } elseif (!preg_match('/^\d{12}$/', $data['nic']) && !preg_match('/^\d{11}V$/', $data['nic'])) {
+                } elseif (!preg_match('/^\d{12}$/', $data['nic']) && !preg_match('/^\d{9}V$/', $data['nic'])) {
                     // Check if the NIC is either 12 digits or 11 digits followed by "V"
-                    $data['nic_err'] = 'NIC must be exactly 12 digits or 11 digits followed by "V" at the end';
+                    $data['nic_err'] = 'NIC must be exactly 12 digits or 9 digits followed by "V" at the end';
                 }
 
 
--- App/views/inc/Components/LoginBox/loginBox.php
+++ App/views/inc/Components/LoginBox/loginBox.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="<?php echo URLROOT; ?>/public/CSS/Components/LoginBox/loginBox.css?v=<?php echo time(); ?>">
 
 <div class="signInContent">
-    <center><h1 class="topic">LOGIN</h1></center>
+    <center><h1 style="color:#43cea2">LOGIN</h1></center>
 
     <div class="login-section">
         
