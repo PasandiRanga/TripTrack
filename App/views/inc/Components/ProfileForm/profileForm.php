8ec01b207a61c75783e4d855fd86554765767d9b
ShashiniMadhu <shashinimadushika413@gmail.com>
2024-11-25 08:23:54 +0530

    style
    profile stylings

--- App/views/inc/Components/ProfileForm/profileForm.php
+++ App/views/inc/Components/ProfileForm/profileForm.php
@@ -10,7 +10,7 @@
                 </div>
                 <div class="detail">
                     <label>Email Address</label>
-                    <input type="email" name="email" value="<?php echo $profile['Email']; ?>">
+                    <input type="mail" name="email" value="<?php echo $profile['Email']; ?>">
                 </div>
                 <div class="detail">
                     <label>Contact Number</label>
