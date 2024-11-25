b48b7df5602592b6e9916db0094be55b7d8c9763
ShashiniMadhu <shashinimadushika413@gmail.com>
2024-11-25 09:48:15 +0530

    add
    add js to contactUs pages

--- App/views/pages/RegisteredUser/contactus.php
+++ App/views/pages/RegisteredUser/contactus.php
@@ -122,6 +122,28 @@
  
     </div>
      
+    <script>
+    const inputs = document.querySelectorAll(".input");
+
+    function focusFunc() {
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+    }
+
+    function blurFunc() {
+    let parent = this.parentNode;
+    if (this.value == "") {
+        parent.classList.remove("focus");
+    }
+    }
+
+    inputs.forEach((input) => {
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", blurFunc);
+    });
+
+    
+</script>
 
    
 
