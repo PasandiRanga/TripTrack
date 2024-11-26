e3392c3e8947319654ffb763d0a601de348f33a1
Pasandi <pasandiranganth@gmail.com>
2024-11-26 19:42:25 +0530

    refactor: update placeholder text in login form and clean up CSS styles

--- App/views/inc/Components/LoginBox/loginBox.php
+++ App/views/inc/Components/LoginBox/loginBox.php
@@ -8,7 +8,7 @@
         <form  class="form" action="<?php echo URLROOT ?>/GuestPages/Login" method="POST" id="loginForm">
 
         <label>Email</label>
-        <input type="email" name="email" id="email" value="<?php echo htmlspecialchars($headerData['email'] ?? ''); ?>" placeholder="Enter username / email">
+        <input type="email" name="email" id="email" value="<?php echo htmlspecialchars($headerData['email'] ?? ''); ?>" placeholder="Enter email">
         <span class="form-invalid">
             <?php echo !empty($headerData['email_err']) ? $headerData['email_err'] : ''; ?>
         </span>
