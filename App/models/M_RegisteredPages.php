642af3c2bfecc551ff23303578b8ae11703af605
ShashiniMadhu <shashinimadushika413@gmail.com>
2025-04-14 16:27:19 +0530

    reviewBox updates
    comment out addReview model and controller

--- App/models/M_RegisteredPages.php
+++ App/models/M_RegisteredPages.php
@@ -584,19 +584,19 @@
                 return [];
             }
         }
-        public function addReview($data) {
-            $this->db->query("INSERT INTO ratings (User_id, License_id, rate, review, Date, Time) 
-                            VALUES (:user_id, :license_id, :rate, :review, :date, :time)");
+        // public function addReview($data) {
+        //     $this->db->query("INSERT INTO ratings (User_id, License_id, rate, review, Date, Time) 
+        //                     VALUES (:user_id, :license_id, :rate, :review, :date, :time)");
 
-            $this->db->bind(':user_id', $data['user_id']);
-            $this->db->bind(':license_id', $data['license_id']);
-            $this->db->bind(':rate', $data['rate']);
-            $this->db->bind(':review', $data['review']);
-            $this->db->bind(':date', $data['date']);
-            $this->db->bind(':time', $data['time']);
+        //     $this->db->bind(':user_id', $data['user_id']);
+        //     $this->db->bind(':license_id', $data['license_id']);
+        //     $this->db->bind(':rate', $data['rate']);
+        //     $this->db->bind(':review', $data['review']);
+        //     $this->db->bind(':date', $data['date']);
+        //     $this->db->bind(':time', $data['time']);
 
-            return $this->db->execute();
-        }
+        //     return $this->db->execute();
+        // }
 
 
     }
